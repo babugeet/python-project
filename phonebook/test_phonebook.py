a6497872417d03aa82dc078fe856b2a6da8ddc3d
babugeet <abhinandhbg@gmail.com>
2023-10-14 23:20:39 +0530

    simulating error scenario to check the git hub actions

--- phonebook/test_phonebook.py
+++ phonebook/test_phonebook.py
@@ -6,15 +6,22 @@ import sqlite3
 # from phonebook import create_table create_db
 DBNAME="test_phonebook.db"
 class Testphonebook(unittest.TestCase):
-    @classmethod
-    def setUp(self) -> None:
-        phonebook.create_db(DBNAME)
+    # @classmethod
+    # def setUp(self) -> None:
+    #     phonebook.create_db(DBNAME)
         # return super().setUp()
+    @classmethod
+    def setUpClass(cls):
+        phonebook.create_db(DBNAME)
+        # print("reached setup class")
+    
+    def test_tes(self):
+         pass
     def test_create_table(self):
         phonebook.create_table(DBNAME)
         conn = sqlite3.connect(DBNAME) 
         c = conn.cursor()
-        stmt = "select * from phonebook"
+        stmt = "select * from phonebookr"
         try:
             c.execute(stmt)
         except sqlite3.OperationalError:
